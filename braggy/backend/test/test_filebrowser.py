308d7cd56084b313b98f5071e58b39ab941e26f3
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2019-02-11 13:45:02 +0100

    Removed static paths in tests

--- braggy/backend/test/test_filebrowser.py
+++ braggy/backend/test/test_filebrowser.py
@@ -14,20 +14,22 @@ async def test_list_dir(client):
     resp = await client.post("/file-browser/list-dir", json={"path": ""})
     data = await(resp.json())
 
+    data_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+
     expected_resp = [
         {
             'text': 'FAE_w1_4_7199.cbf',
-            'fpath': '/home/marcus/projects/braggy/braggy/backend/test/data/FAE_w1_4_7199.cbf',
+            'fpath': data_root + '/FAE_w1_4_7199.cbf',
             'isLeaf': True
         },
         {
             'text': 'in16c_010001.cbf',
-            'fpath': '/home/marcus/projects/braggy/braggy/backend/test/data/in16c_010001.cbf',
+            'fpath': data_root + '/in16c_010001.cbf',
             'isLeaf': True
         },
         {
             'text': 'FAE_w1_4_7186.cbf',
-            'fpath': '/home/marcus/projects/braggy/braggy/backend/test/data/FAE_w1_4_7186.cbf',
+            'fpath': data_root + '/FAE_w1_4_7186.cbf',
             'isLeaf': True
         }
     ]

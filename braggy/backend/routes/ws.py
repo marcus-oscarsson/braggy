17e7ae0264f956f4ec7ebac3261ce8145188fd0d
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2019-02-21 15:30:52 +0100

    Removed and missing files

---
+++ braggy/backend/routes/ws.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+import socketio
+import logging
+
+sio = socketio.AsyncServer(async_mode='aiohttp')
+
+
+@sio.on('connect')
+def connect(sid, environ):
+    logging.debug("Websocket connected %s" % sid)
+
+
+@sio.on('chat message')
+async def message(sid, data):
+    print("message ", data)
+    await sio.emit('repy', room=sid)
+
+
+@sio.on('disconnect')
+def disconnect(sid):
+    print("Websocket disconnected %s" % sid)
--- braggy/ui/imageview/image-download-worker.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-
-// Post data to parent thread
-this.postMessage({ foo: 'foo' });
-
-// Respond to message from parent thread
-this.addEventListener('message', (event) => console.log(event));

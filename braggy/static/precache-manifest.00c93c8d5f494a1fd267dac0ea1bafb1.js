a14459f9e56810b7cb3a664daeebff01ac3d3a73
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2019-03-04 11:31:39 +0100

    Added static resources

---
+++ braggy/static/precache-manifest.00c93c8d5f494a1fd267dac0ea1bafb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02d611faa1f0d5e258cd",
+    "url": "/static/js/runtime~main.02d611fa.js"
+  },
+  {
+    "revision": "153913a1418444bbcbf1",
+    "url": "/static/js/main.153913a1.chunk.js"
+  },
+  {
+    "revision": "58e102e25c623172fb37",
+    "url": "/static/js/1.58e102e2.chunk.js"
+  },
+  {
+    "revision": "153913a1418444bbcbf1",
+    "url": "/static/css/main.06c9e9ce.chunk.css"
+  },
+  {
+    "revision": "10d21a39dd5dbe1703191daf77815a35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c4492ba9b4b0d8b76d960634ea43b09",
+    "url": "/2bb43da623c9e2f18afd.worker.js"
+  }
+];

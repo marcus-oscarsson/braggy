f08884169749d47292cd6b9d50647482e1706767
Marcus Oskarsson <oscarsso@linoscarsson.esrf.fr>
2020-12-03 15:01:47 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with io.open('braggy/__init__.py', 'rt', encoding='utf8') as f:
 setup(
     name='BSX3',
     version=version,
-    url='https://www.palletsprojects.com/p/flask/',
+    url='https://github.com/marcus-oscarsson/braggy',
     license='GPL',
     author='Marcus Oskarsson',
     author_email='oscarsso@esrf.fr',
@@ -28,23 +28,13 @@ setup(
     platforms='Linux',
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.',
     install_requires=[
-        'gevent',
-        'flask',
-        'python-socketio',
-        'python-engineio',
-        'flask-socketio',
-        'gevent-websocket',
     ],
     extras_require={
         'dev': [
             'pytest>=3',
             'pytest-cov',
             'coverage',
-            'sphinx',
         ],
-        'docs': [
-            'sphinx',
-        ]
     },
     classifiers=[
         'Programming Language :: Python :: 3.5',
